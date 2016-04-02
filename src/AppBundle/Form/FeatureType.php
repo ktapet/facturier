6336f6fd91043c74df1e176ece8728e5b659dd08
rusanu.ramona <rusanu.ramona@gmail.com>
2016-04-02 15:25:22 +0300

    added crud for product and feature

---
+++ src/AppBundle/Form/FeatureType.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace AppBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class FeatureType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('value')
+            ->add('datCre', 'datetime')
+            ->add('datUpd', 'datetime')
+            ->add('products')
+            ->add('name')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolver $resolver
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'AppBundle\Entity\Feature'
+        ));
+    }
+}
