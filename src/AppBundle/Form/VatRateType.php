1a9f2c5bfd3c745b70ad7803fad8243c5b5c4fee
Ramona <ramona.rusanu@webdevro.com>
2016-04-08 14:34:29 +0300

    changed the structure of links; crud for vat rate

---
+++ src/AppBundle/Form/VatRateType.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace AppBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class VatRateType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('code')
+            ->add('description')
+            ->add('rate')
+//            ->add('datCre', 'datetime')
+//            ->add('datUpd', 'datetime')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolver $resolver
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'AppBundle\Entity\VatRate'
+        ));
+    }
+}
